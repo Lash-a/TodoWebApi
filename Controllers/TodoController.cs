0ca7df49e48528ae13d5462a091b82fe30f51d65
Lash-a <64647074+Lash-a@users.noreply.github.com>
2021-06-21 22:54:07 +0400

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Http;
+using TodoWebApi.Models;
+using TodoWebApi.Repositories;
+
+namespace TodoWebApi.Controllers
+{
+    public class TodoController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult SaveTodo(Todo todo)
+        {
+            //იქმნება ახალი TodoRepository ობიექტი რომელიც შემდეგ გადადის ამ კლასის კონსტრუქტორში
+            var todoRepository = new TodoRepository();
+            // გამოვიძახეთ todoRepository კლასის saveTodo მეთოდი რომელსაც გადავეცით todo კლასი 
+            // todo კლასში შედის ID Description და Done ცვლადები
+            todoRepository.SaveTodo(todo);
+
+            //თუ მოაღწია აქამდე ესეიგი ერრორი არ ამოვარდნილა ფუნქცია გაეშვა წარმატებით და დააბრუნოს სტატუსი 200
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetTodoList()
+        {
+            var todoRepository = new TodoRepository();
+            var todoList = todoRepository.GetTodoList();
+            return Ok(todoList);
+        }
+
+        [HttpPost]
+        public IHttpActionResult ChangeDoneOrNot(Todo todo)
+        {
+            var todoRepository = new TodoRepository();
+            todoRepository.ChangeDoneOrNot(todo);
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult DeleteTodo([FromBody]int id)
+        {
+            var todoRepository = new TodoRepository();
+            todoRepository.DeleteTodo(id);
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult UpdateTodo(Todo todo)
+        {
+            var todoRepository = new TodoRepository();
+            todoRepository.UpdateTodo(todo);
+
+            return Ok();
+        }
+    }
+}
---
+++ Repositories/TodoRepository.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using TodoWebApi.Models;
+
+namespace TodoWebApi.Repositories
+{
+    public class TodoRepository
+    {
+        //es cvladi gansazgvravs romel monacemta bazas uertdeba es klasi
+        private SqlConnection conn;
+
+        public TodoRepository()
+        {
+            //გადმოვიდა TodoController კლასიდან სადაც შეიქმნა TodoRepository-ს ახალი ობიექტი და გაუშვა ეს კონსტრუქტორი
+            //კონსტრუქტორში დამყარდა კავშირი მონაცემთა ბაზასთან. ეს კავშირი განისაზღვრა web.config ფაილის კონფიგურაციიდან და მიენიჭა conn ცვლადს
+            //რომელსაც შემდეგ გამოვიყენებთ კავშირის დასახურად.
+            string connStr = ConfigurationManager.ConnectionStrings["TodoConnStr"].ConnectionString;
+            conn = new SqlConnection(connStr);
+        }
+
+        public void SaveTodo(Todo todo)
+        {
+            // კონსტრუქტორში შეიძლება მივუთითოთ cmd.CommandText და ასევე cmd.Connection ცვლადების მნიშვნელობები
+            //თუმცა უმჯობესია დაკავშირება დავიწყოთ გვიან
+            var cmd = new SqlCommand();
+            // cmd.CommandText = "dbo.SaveTodo";
+            //განსაზღვრავს პროცედურას რომლის გაშვებაც გვინდა სქლ-ში
+            cmd.CommandText = "dbo.Todo_SaveTodo";
+            // cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = todo.Description;
+            //განსაზღვრავს ცვლადებს ანიჭებს datatypes და შემდეგ უტოლებს c#-ის ცვლადს
+            cmd.Parameters.Add("@Description", SqlDbType.NVarChar).Value = todo.Description;
+            // cmd.Parameters.Add("@Status", SqlDbType.Int).Value = todo.Done;
+            cmd.Parameters.Add("@Done", SqlDbType.Int).Value = todo.Done;
+            //განსაზღვრავს ბრძანების ტიპს
+            cmd.CommandType = CommandType.StoredProcedure;
+            //მიუთითებს თუ რომელ ბაზასთან გვსურს დაკავშირება
+            cmd.Connection = conn;
+            //ამყარებს ამ ბაზასთან კავშირს
+            cmd.Connection.Open(); // conn.Open();
+            //უშვებს პროცედურას 
+            cmd.ExecuteNonQuery();
+            //წყვეტს ბაზასთან კავშირს
+            conn.Close();
+        }
+
+
+        public void ChangeDoneOrNot(Todo todo)
+        {
+            var cmd = new SqlCommand();
+            cmd.CommandText = "dbo.Todo_ChangeDoneOrNot";
+            cmd.Parameters.Add("@Done", SqlDbType.Bit).Value = todo.Done;
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = todo.ID;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+
+        public void DeleteTodo(int id)
+        {
+            var cmd = new SqlCommand("dbo.Todo_DeleteTodo", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = id;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+
+            //var cmd = new SqlCommand();
+            //cmd.CommandText = "dbo.Todo_DeleteTodo";
+
+            //cmd.Parameters.Add("@ID", SqlDbType.Int).Value = todo.ID;
+
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Connection = conn;
+
+            //cmd.Connection.Open(); // conn.Open();
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+
+        }
+
+
+        public void UpdateTodo(Todo todo)
+        {
+            var cmd = new SqlCommand("dbo.Todo_UpdateTodo", conn);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = todo.ID;
+            cmd.Parameters.Add("@Description", SqlDbType.NVarChar).Value = todo.Description;
+            cmd.Parameters.Add("@Done", SqlDbType.Bit).Value = todo.Done;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+
+
+
+        public List<Todo> GetTodoList()
+        {
+            var todoList = new List<Todo>();
+
+            var cmd = new SqlCommand();
+            // cmd.CommandText = "dbo.GetTodoes";
+            cmd.CommandText = "dbo.Todo_GetAllTodo";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+
+            cmd.Connection.Open();
+
+            var reader = cmd.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                var todo = new Todo();
+                todo.ID = int.Parse(reader["ID"].ToString());
+                // todo.Description = reader["Name"].ToString();
+                todo.Description = reader["Description"].ToString();
+                // todo.Done = int.Parse(reader["Status"].ToString()) > 0;
+                todo.Done = bool.Parse(reader["Done"].ToString());
+                todoList.Add(todo);
+            }
+            conn.Close();
+
+            return todoList;
+        }
+
+
+    }
+}
